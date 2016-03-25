f1c0fc656519b3c74d38b228957fcc2d95005c5e
GIT User <xiaoye.xu@outlook.com>
2016-03-25 22:28:24 +0800

    updated

--- crm_site/settings.py
+++ crm_site/settings.py
@@ -149,7 +149,7 @@ DEFAULT_CHARSET = 'utf-8'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/crmstatic/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "crmstatic"),
