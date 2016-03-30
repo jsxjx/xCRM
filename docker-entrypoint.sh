b58ab6e6c9d9680142c14378ce2260d99f8ec006
GIT User <xiaoye.xu@outlook.com>
2016-03-30 16:05:56 +0800

    updated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
-/code/manage.py syncdb --noinput
+# Django 1.8
+#/code/manage.py syncdb --noinput
+
+# Django 1.9 since syncdb is deprecated
+/code/manage.py makemigrations
+/code/manage.py migrate
 /usr/local/bin/gunicorn crm_site.wsgi:application -w 2 -b :8000
 #python /code/manage.py runserver 0.0.0.0:8000
